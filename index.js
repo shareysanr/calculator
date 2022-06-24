3e5bd5dbdddbf4fa5abe533e5714fb7c3a791d2d
shareysanr <shareysanr1@gmail.com>
2022-06-23 23:50:53 -0400

    Adding the option to remove characters

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 let display = document.getElementById("display");
 
-let buttons = Array.from(document.getElementById("buttons"));
+let buttons = Array.from(document.getElementsByClassName("buttons"));
+
+buttons.map( button => {
+    button.addEventListener('click', (e) => {
+        switch(e.target.innerText) {
+            case 'C':
+                display.innerText = "";
+                break;
+            case '←':
+                if (display.innerText) {
+                    display.innerText = display.innerText.slice(0, -1)
+                }
+                break;
+            default:
+                display.innerText += e.target.innerText;
+        }
+    })
+})
